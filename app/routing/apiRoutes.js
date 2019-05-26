15dfd9c93c3d908115e22c85cb3fc1182cc44d94
Jacob Dahlberg <dahlberg2009@gmail.com>
2019-05-26 00:18:31 -0500

    completed project

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,26 @@
+var friendData = require('../data/friends');
+
+module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res) {
+
+        res.json(friendData);
+
+    });
+
+    app.post("/api/friends", function(req, res) {
+
+        friendData.push(req.body);
+        res.json(true);
+
+    });
+
+    app.post("/api/clear", function(req, res) {
+
+        friendData.length = [];
+
+        res.json({ ok: true});
+
+    });
+
+};
